0e04ea7fa4a04f0c71cc30d508fd283edf380a6a
José David Frías Jiménez <76705873+jdfj94@users.noreply.github.com>
2020-12-30 22:28:25 +0100

    Cambios

--- index.php
+++ index.php
@@ -77,6 +77,8 @@
             <div class="footer__social">
                 <a href="https://www.linkedin.com/in/jose-david-frias-jimenez/" target="blank" class="footer__link"><i
                         class='bx bxl-linkedin-square'></i></a>
+                <a href="https://github.com/jdfj94" target="blank"
+                        class="home__social-link"><i class='bx bxl-github'></i></a>
             </div>
         </div>
     </footer>
